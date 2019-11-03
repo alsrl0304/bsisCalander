47c83377f7d5c4eebc2f493085468d257a81ad3f
alsrl0304 <alsrl0304@gmail.com>
2019-11-04 00:00:46 +0900

    두번째 페이지 틀 추가
    Signed-off-by: alsrl0304 <alsrl0304@gmail.com>

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var todolistRouter = require('./routes/todolist');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/result', todolistRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/todolist.js
+++ routes/todolist.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BSIS_TODO' });
+router.get('/todolist', function(req, res, next) {
+  res.render('todolist', { title: 'BSIS_TODO_LIST' });
 });
 
 module.exports = router;
