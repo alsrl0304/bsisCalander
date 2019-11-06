78130ad774234bf053db5518bbb687169d6bcfa8
alsrl0304 <alsrl0304@gmail.com>
2019-11-06 17:29:41 +0900

    todolist

--- routes/todolist.js
+++ routes/todolist.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const query_grade = req.query.grade;
+  const query_class = req.query.class;
   res.render('todolist', { title: 'BSIS_TODO_LIST',
-    grade: req.query['grade'],
-    class: req.query['class']
+    query_grade, query_class
   });
 });
 
