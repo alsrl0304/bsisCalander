fa248d5da52189e02b4811a4a17c18cfcb025bd1
alsrl0304 <alsrl0304@gmail.com>
2019-11-06 14:24:01 +0900

    ES2018

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var todolistRouter = require('./routes/todolist');
+const indexRouter = require('./routes/index');
+const todolistRouter = require('./routes/todolist');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
