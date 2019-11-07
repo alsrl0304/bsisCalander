fff1690b56beaf878b7fef88676fbfab80ccc704
alsrl0304 <alsrl0304@gmail.com>
2019-11-07 16:48:40 +0900

    경미한 수정

--- routes/todolist.js
+++ routes/todolist.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const query_grade = req.query.grade;
   const query_class = req.query.class;
   res.render('todolist', { title: 'BSIS_TODO_LIST',
-    query_grade, query_class
-  });
+    query_grade, query_class });
 });
 
 module.exports = router;
