9a3b2634277f2da7e49945551e7a61365e8e641d
alsrl0304 <alsrl0304@gmail.com>
2020-05-01 22:21:01 +0900

    Babel, Webpack, React Setups
    Signed-off-by: alsrl0304 <alsrl0304@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  entry: { index: "./src/scripts/index.jsx" },
+  output: {
+    path: path.resolve(__dirname, "public/javascripts"),
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [path.resolve(__dirname, "src/scripts")],
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  mode: "development",
+};
